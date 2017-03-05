e6697b1edca7f354afc5bc07605d812ca88eec4e
Gabor Fekete <feheren.fekete@gmail.com>
2017-03-05 22:18:22 +0200

    Remove unused ViewPager support.
    But leave the page support in the DataModel.

--- app/src/main/java/net/feheren_fekete/applist/ApplistActivity.java
+++ app/src/main/java/net/feheren_fekete/applist/ApplistActivity.java
@@ -3,17 +3,13 @@ package net.feheren_fekete.applist;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -24,20 +20,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import net.feheren_fekete.applist.model.BadgeStore;
 import net.feheren_fekete.applist.model.DataModel;
 import net.feheren_fekete.applist.shortcutbadge.BadgeUtils;
 import net.feheren_fekete.applist.utils.FileUtils;
-import net.feheren_fekete.applist.utils.RunnableWithArg;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import java.io.File;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -57,9 +50,6 @@ public class ApplistActivity extends AppCompatActivity {
     private BadgeStore mBadgeStore;
     private AppBarLayout mAppBarLayout;
     private Toolbar mToolbar;
-    private ViewPager mViewPager;
-    private TabLayout mTabLayout;
-    private ApplistPagerAdapter2 mPagerAdapter;
     private Menu mMenu;
     private SearchView mSearchView;
 
@@ -82,23 +72,6 @@ public class ApplistActivity extends AppCompatActivity {
         mToolbar.setTitle(R.string.toolbar_title);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        mPagerAdapter = new ApplistPagerAdapter2(
-                getSupportFragmentManager(), mDataModel, mIconCache);
-        mViewPager.setAdapter(mPagerAdapter);
-        mViewPager.addOnPageChangeListener(mPageChangeListener);
-        mViewPager.requestFocus();
-
-        mTabLayout = (TabLayout) findViewById(R.id.tabs);
-        // WORKAROUND: We must post setupWithViewPager() due to a bug:
-        // https://code.google.com/p/android/issues/detail?id=180462
-        mTabLayout.post(new Runnable() {
-            @Override
-            public void run() {
-                mTabLayout.setupWithViewPager(mViewPager);
-            }
-        });
 
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(Intent.ACTION_PACKAGE_ADDED);
@@ -140,29 +113,17 @@ public class ApplistActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                mViewPager.requestFocus();
-            }
-        }, 500);
-
         EventBus.getDefault().register(this);
-
         mPackageStateReceiver.onReceive(this, null);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
         if (mSearchView != null) {
             hideKeyboardFrom(this, mSearchView);
             mSearchView.setIconified(true);
-            mViewPager.requestFocus();
         }
-
         EventBus.getDefault().unregister(this);
     }
 
@@ -196,7 +157,7 @@ public class ApplistActivity extends AppCompatActivity {
     public boolean onPrepareOptionsMenu(Menu menu) {
         boolean isChangingOrder = false;
         boolean isFilteredByName = false;
-        ApplistFragment fragment = mPagerAdapter.getCurrentPageFragment();
+        ApplistFragment fragment = getApplistFragment();
         if (fragment != null) {
             isChangingOrder = fragment.isChangingOrder();
             isFilteredByName = fragment.isFilteredByName();
@@ -204,19 +165,16 @@ public class ApplistActivity extends AppCompatActivity {
         if (isChangingOrder) {
             menu.findItem(R.id.action_search_app).setVisible(false);
             menu.findItem(R.id.action_create_section).setVisible(false);
-            menu.findItem(R.id.action_create_page).setVisible(false);
             menu.findItem(R.id.action_done).setVisible(true);
             menu.findItem(R.id.action_settings).setVisible(false);
         } else if (isFilteredByName) {
             menu.findItem(R.id.action_search_app).setVisible(true);
             menu.findItem(R.id.action_create_section).setVisible(false);
-            menu.findItem(R.id.action_create_page).setVisible(false);
             menu.findItem(R.id.action_done).setVisible(false);
             menu.findItem(R.id.action_settings).setVisible(false);
         } else {
             menu.findItem(R.id.action_search_app).setVisible(true);
             menu.findItem(R.id.action_create_section).setVisible(true);
-            menu.findItem(R.id.action_create_page).setVisible(false);
             menu.findItem(R.id.action_done).setVisible(false);
             menu.findItem(R.id.action_settings).setVisible(true);
         }
@@ -228,17 +186,13 @@ public class ApplistActivity extends AppCompatActivity {
         boolean isHandled = false;
         int id = item.getItemId();
 
-        ApplistFragment fragment = mPagerAdapter.getCurrentPageFragment();
+        ApplistFragment fragment = getApplistFragment();
         if (fragment != null) {
             isHandled = fragment.handleMenuItem(id);
         }
 
         if (!isHandled) {
             switch (id) {
-                case R.id.action_create_page:
-                    createPage();
-                    isHandled = true;
-                    break;
                 case R.id.action_settings:
                     showSettings();
                     isHandled = true;
@@ -277,30 +231,10 @@ public class ApplistActivity extends AppCompatActivity {
         }
     };
 
-    private ViewPager.OnPageChangeListener mPageChangeListener = new ViewPager.OnPageChangeListener() {
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            ApplistFragment fragment = mPagerAdapter.getCurrentPageFragment();
-            if (fragment != null) {
-                if (fragment.isFilteredByName()) {
-                    stopFilteringByName(fragment);
-                }
-            }
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-        }
-    };
-
     private SearchView.OnFocusChangeListener mSearchFocusListener = new View.OnFocusChangeListener() {
         @Override
         public void onFocusChange(View v, boolean hasFocus) {
-            ApplistFragment fragment = mPagerAdapter.getCurrentPageFragment();
+            ApplistFragment fragment = getApplistFragment();
             if (fragment != null) {
                 if (!hasFocus) {
                     stopFilteringByName(fragment);
@@ -319,7 +253,7 @@ public class ApplistActivity extends AppCompatActivity {
 
         @Override
         public boolean onQueryTextChange(String newText) {
-            ApplistFragment fragment = mPagerAdapter.getCurrentPageFragment();
+            ApplistFragment fragment = getApplistFragment();
             if (fragment != null) {
                 if (newText == null || newText.isEmpty()) {
                     fragment.setNameFilter("");
@@ -350,7 +284,7 @@ public class ApplistActivity extends AppCompatActivity {
             mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    ApplistFragment fragment = mPagerAdapter.getCurrentPageFragment();
+                    ApplistFragment fragment = getApplistFragment();
                     if (fragment != null) {
                         fragment.cancelChangingOrder();
                     }
@@ -394,7 +328,7 @@ public class ApplistActivity extends AppCompatActivity {
         mHandler.post(new Runnable() {
             @Override
             public void run() {
-                updatePageFragments();
+                updateApplistFragment();
             }
         });
     }
@@ -405,7 +339,7 @@ public class ApplistActivity extends AppCompatActivity {
         mHandler.post(new Runnable() {
             @Override
             public void run() {
-                updatePageFragments();
+                updateApplistFragment();
             }
         });
     }
@@ -438,7 +372,7 @@ public class ApplistActivity extends AppCompatActivity {
         }, Task.UI_THREAD_EXECUTOR);
     }
 
-    private void loadPageFragments() {
+    private void loadApplistFragment() {
         Task.callInBackground(new Callable<List<String>>() {
             @Override
             public List<String> call() throws Exception {
@@ -447,24 +381,13 @@ public class ApplistActivity extends AppCompatActivity {
         }).continueWith(new Continuation<List<String>, Void>() {
             @Override
             public Void then(Task<List<String>> task) throws Exception {
-                mPagerAdapter.clearPageNames();
                 List<String> pageNames = task.getResult();
-                mPagerAdapter.setPageNames(pageNames);
-                mPagerAdapter.notifyDataSetChanged();
-                mHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        mPagerAdapter.forEachPageFragment(new RunnableWithArg<ApplistFragment>() {
-                            @Override
-                            public void run(ApplistFragment fragment) {
-                                fragment.setListener(mFragmentListener);
-                                fragment.update();
-                            }
-                        });
-                    }
-                }, 1000);
-                hack();
-                mTabLayout.setVisibility((pageNames.size() == 1) ? View.GONE : View.VISIBLE);
+                if (!pageNames.isEmpty()) {
+                    showApplistFragment(pageNames.get(0));
+                } else {
+                    // TODO: log: report
+                    Log.d(TAG, "No pages found!");
+                }
                 return null;
             }
         }, Task.UI_THREAD_EXECUTOR);
@@ -481,112 +404,26 @@ public class ApplistActivity extends AppCompatActivity {
         });
     }
 
-    private void updatePageFragments() {
+    private void updateApplistFragment() {
         if (mDataModel.getPageCount() == 0) {
             addDefaultPage();
         } else {
-            loadPageFragments();
+            loadApplistFragment();
         }
     }
 
-    private void hack() {
-        mTabLayout.post(new Runnable() {
-            @Override
-            public void run() {
-                mTabLayout.setupWithViewPager(mViewPager);
-            }
-        });
-        mTabLayout.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                LinearLayout tabStrip = (LinearLayout) mTabLayout.getChildAt(0);
-                final List<String> pageNames = mPagerAdapter.getPageNames();
-                for (int i = 0; i < tabStrip.getChildCount(); i++) {
-                    final int finalI = i;
-                    tabStrip.getChildAt(i).setOnLongClickListener(new View.OnLongClickListener() {
-                        @Override
-                        public boolean onLongClick(View v) {
-                            onPageLongTapped(pageNames.get(finalI));
-                            return false;
-                        }
-                    });
-                }
-            }
-        }, 2000);
+    private void showApplistFragment(String pageName) {
+        ApplistFragment fragment = ApplistFragment.newInstance(pageName, mDataModel, mIconCache);
+        fragment.setListener(mFragmentListener);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment, ApplistFragment.class.getName())
+                .commit();
     }
 
-    private static final int PAGE_MENU_RENAME = 0;
-    private static final int PAGE_MENU_DELETE = 1;
-
-    private void onPageLongTapped(final String pageName) {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setTitle(pageName);
-        alertDialogBuilder.setItems(R.array.page_menu, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case PAGE_MENU_RENAME:
-                        renamePage(pageName);
-                        break;
-                    case PAGE_MENU_DELETE:
-                        deletePage(pageName);
-                        break;
-                }
-            }
-        });
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
-    }
-
-    private void renamePage(final String pageName) {
-        ApplistDialogs.textInputDialog(
-                this, R.string.page_name, pageName, null,
-                new RunnableWithArg<String>() {
-                    @Override
-                    public void run(final String newPageName) {
-                        Task.callInBackground(new Callable<Void>() {
-                            @Override
-                            public Void call() throws Exception {
-                                mDataModel.setPageName(pageName, newPageName);
-                                return null;
-                            }
-                        });
-                    }
-                });
-    }
-
-    private void deletePage(final String pageName) {
-        ApplistDialogs.questionDialog(
-                this, R.string.remove_page,
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        Task.callInBackground(new Callable<Void>() {
-                            @Override
-                            public Void call() throws Exception {
-                                mDataModel.removePage(pageName);
-                                return null;
-                            }
-                        });
-                    }
-                });
-    }
-
-    private void createPage() {
-        ApplistDialogs.textInputDialog(
-                this, R.string.page_name, "", null,
-                new RunnableWithArg<String>() {
-                    @Override
-                    public void run(final String pageName) {
-                        Task.callInBackground(new Callable<Void>() {
-                            @Override
-                            public Void call() throws Exception {
-                                mDataModel.addNewPage(pageName);
-                                return null;
-                            }
-                        });
-                    }
-                });
+    private ApplistFragment getApplistFragment() {
+        return (ApplistFragment) getSupportFragmentManager().findFragmentByTag(
+                ApplistFragment.class.getName());
     }
 
 }
