a521311a569b7cd98fb6238d15ffaad082d91e30
Gabor Fekete <feheren.fekete@gmail.com>
2017-06-20 09:20:55 +0300

    Add light transparent theme.
    # Conflicts:
    #	app/src/main/res/values-fi/strings.xml
    #	app/src/main/res/values-hu/strings.xml

--- app/src/main/java/net/feheren_fekete/applist/settings/SettingsUtils.java
+++ app/src/main/java/net/feheren_fekete/applist/settings/SettingsUtils.java
@@ -4,40 +4,44 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.util.ArrayMap;
 
+import net.feheren_fekete.applist.ApplistLog;
 import net.feheren_fekete.applist.R;
-import net.feheren_fekete.applist.settings.SettingsActivity;
+
+import java.util.Map;
 
 public class SettingsUtils {
 
+    private static Map<String, Integer> sColorThemes;
+
+    public static Map<String, Integer> createColorThemeMap(Context context) {
+        Map<String, Integer> result = new ArrayMap<>();
+        result.put(context.getString(R.string.color_theme_value_transparent_light), R.style.MyThemeTransparentLight);
+        result.put(context.getString(R.string.color_theme_value_transparent_dark), R.style.MyThemeTransparentDark);
+        result.put(context.getString(R.string.color_theme_value_black), R.style.MyThemeBlack);
+        result.put(context.getString(R.string.color_theme_value_brown), R.style.MyThemeBrown);
+        result.put(context.getString(R.string.color_theme_value_blue_grey), R.style.MyThemeBlueGrey);
+        result.put(context.getString(R.string.color_theme_value_deep_purple), R.style.MyThemeDeepPurple);
+        result.put(context.getString(R.string.color_theme_value_indigo), R.style.MyThemeIndigo);
+        result.put(context.getString(R.string.color_theme_value_red), R.style.MyThemeRed);
+        result.put(context.getString(R.string.color_theme_value_green), R.style.MyThemeGreen);
+        return result;
+    }
+
     public static void applyColorTheme(Activity activity) {
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(activity.getApplicationContext());
-        String colorThemeName = sharedPref.getString(SettingsActivity.PREF_KEY_COLOR_THEME, "Brown");
-        switch (colorThemeName) {
-            case "Transparent":
-                activity.setTheme(R.style.MyThemeTransparent);
-                break;
-            case "Black":
-                activity.setTheme(R.style.MyThemeBlack);
-                break;
-            case "Brown":
-                activity.setTheme(R.style.MyThemeBrown);
-                break;
-            case "Blue Grey":
-                activity.setTheme(R.style.MyThemeBlueGrey);
-                break;
-            case "Deep Purple":
-                activity.setTheme(R.style.MyThemeDeepPurple);
-                break;
-            case "Indigo":
-                activity.setTheme(R.style.MyThemeIndigo);
-                break;
-            case "Red":
-                activity.setTheme(R.style.MyThemeRed);
-                break;
-            case "Green":
-                activity.setTheme(R.style.MyThemeGreen);
-                break;
+        if (sColorThemes == null) {
+            sColorThemes = createColorThemeMap(activity);
+        }
+
+        final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(activity.getApplicationContext());
+        final String defaultColorThemeName = activity.getString(R.string.color_theme_value_default);
+        final String colorThemeName = sharedPref.getString(SettingsActivity.PREF_KEY_COLOR_THEME, defaultColorThemeName);
+        final Integer colorThemeId = sColorThemes.get(colorThemeName);
+        if (colorThemeId != null) {
+            activity.setTheme(colorThemeId);
+        } else {
+            ApplistLog.getInstance().log(new RuntimeException("Invalid color theme: " + colorThemeName));
         }
     }
 
