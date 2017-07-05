4b3b307862f5e93c9557356c86083c6c494225f7
Gabor Fekete <feheren.fekete@gmail.com>
2017-07-05 15:19:57 +0300

    Pick first widget page.

--- app/src/main/java/net/feheren_fekete/applist/widgetpage/WidgetHelper.java
+++ app/src/main/java/net/feheren_fekete/applist/widgetpage/WidgetHelper.java
@@ -114,6 +114,7 @@ public class WidgetHelper {
                                 return null;
                             }
                         });
+                        break;
                     }
                 }
                 return null;
