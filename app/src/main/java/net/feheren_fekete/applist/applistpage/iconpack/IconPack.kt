df4b534e02169f3e13ecaef6592aaa9c6b1f135d
Gabor Fekete <feheren.fekete@gmail.com>
2019-04-10 23:37:37 +0300

    Add basic IconPacksLiveData

---
+++ app/src/main/java/net/feheren_fekete/applist/applistpage/iconpack/IconPack.kt
@@ -0,0 +1,6 @@
+package net.feheren_fekete.applist.applistpage.iconpack
+
+import android.content.ComponentName
+
+data class IconPack(val name: String,
+                    val componentName: ComponentName)
