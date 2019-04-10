92aea892443498dc3dfb9f7f66b6fe3c5d6ef612
Gabor Fekete <feheren.fekete@gmail.com>
2019-04-10 23:37:12 +0300

    Refactor IconPackHelper

--- app/src/main/java/net/feheren_fekete/applist/applistpage/iconpack/IconPackHelper.kt
+++ app/src/main/java/net/feheren_fekete/applist/applistpage/iconpack/IconPackHelper.kt
@@ -20,7 +20,9 @@ class IconPackHelper {
                  iconPackPackageName: String,
                  componentName: ComponentName): Bitmap? {
         val iconPackResources = packageManager.getResourcesForApplication(iconPackPackageName)
-        return loadBitmap(iconPackPackageName, iconPackResources, null, componentName.toString())
+        return getIconDrawableName(iconPackResources, iconPackPackageName, componentName)?.let {
+            return@let loadBitmap(iconPackPackageName, iconPackResources, it)
+        }
     }
 
     fun createFallbackIcon(packageManager: PackageManager,
@@ -36,16 +38,16 @@ class IconPackHelper {
         iconPaint.isAntiAlias = true
 
         // Draw the background
-        val backBitmap = loadBitmap(iconPackPackageName, iconPackResources, "iconback", null)
-        if (backBitmap != null) {
-            iconCanvas.drawBitmap(backBitmap, getResizeMatrix(backBitmap, width, height), iconPaint)
+        getFallbackIconDrawableName(iconPackResources, iconPackPackageName, "iconback")?.let {
+            loadBitmap(iconPackPackageName, iconPackResources, it)?.let { backBitmap ->
+                iconCanvas.drawBitmap(backBitmap, getResizeMatrix(backBitmap, width, height), iconPaint)
+            }
         }
 
         // Prepare a scaled-down version of the original icon
         val scaledOriginalIcon = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val scaledOriginalIconCanvas = Canvas(scaledOriginalIcon)
-        val scaleResourceName = getResourceName(iconPackResources, iconPackPackageName, "scale", null)
-        val scale = scaleResourceName?.toFloat() ?: 1.0f
+        val scale = getScaleFactor(iconPackResources, iconPackPackageName)
         val scaledWidth = (width * scale)
         val scaledHeight = (height * scale)
         val m = getResizeMatrix(originalIcon, scaledWidth.toInt(), scaledHeight.toInt())
@@ -55,21 +57,23 @@ class IconPackHelper {
         scaledOriginalIconCanvas.drawBitmap(originalIcon, m, scaledOriginalPaint)
 
         // Apply mask on top of the scaled original
-        val maskBitmap = loadBitmap(iconPackPackageName, iconPackResources, "iconmask", null)
-        if (maskBitmap != null) {
-            val maskPaint = Paint(Paint.FILTER_BITMAP_FLAG)
-            maskPaint.isAntiAlias = true
-            maskPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
-            scaledOriginalIconCanvas.drawBitmap(maskBitmap, getResizeMatrix(maskBitmap, width, height), maskPaint)
+        getFallbackIconDrawableName(iconPackResources, iconPackPackageName, "iconmask")?.let {
+            loadBitmap(iconPackPackageName, iconPackResources, it)?.let { maskBitmap ->
+                val maskPaint = Paint(Paint.FILTER_BITMAP_FLAG)
+                maskPaint.isAntiAlias = true
+                maskPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
+                scaledOriginalIconCanvas.drawBitmap(maskBitmap, getResizeMatrix(maskBitmap, width, height), maskPaint)
+            }
         }
 
         // Draw the scaled original icon on top of the background
         iconCanvas.drawBitmap(scaledOriginalIcon, 0.0f, 0.0f, iconPaint)
 
         // Put the "upon" layer on top
-        val uponBitmap = loadBitmap(iconPackPackageName, iconPackResources, "iconupon", null)
-        if (uponBitmap != null) {
-            iconCanvas.drawBitmap(uponBitmap, getResizeMatrix(uponBitmap, width, height), iconPaint)
+        getFallbackIconDrawableName(iconPackResources, iconPackPackageName, "iconupon")?.let {
+            loadBitmap(iconPackPackageName, iconPackResources, it)?.let { uponBitmap ->
+                iconCanvas.drawBitmap(uponBitmap, getResizeMatrix(uponBitmap, width, height), iconPaint)
+            }
         }
 
         return icon
@@ -77,48 +81,78 @@ class IconPackHelper {
 
     private fun loadBitmap(iconPackPackageName: String,
                            iconPackResources: Resources,
-                           resourceName: String?,
-                           componentName: String?): Bitmap? {
-        val resourceNameInIconPack = getResourceName(
-                iconPackResources, iconPackPackageName, resourceName, componentName) ?: return null
-        val resourceId = iconPackResources.getIdentifier(resourceNameInIconPack, "drawable", iconPackPackageName)
+                           resourceName: String): Bitmap? {
+        val resourceId = iconPackResources.getIdentifier(resourceName, "drawable", iconPackPackageName)
         return BitmapFactory.decodeResource(iconPackResources, resourceId, uniformOptions)
     }
 
-    private fun getResourceName(resources: Resources,
-                                packageName: String,
-                                resourceName: String?,
-                                componentName: String?): String? {
-        val xrp: XmlResourceParser
-        var resource: String? = null
-        try {
-            val resourceValue = resources.getIdentifier("appfilter", "xml", packageName)
-            if (resourceValue != 0) {
-                xrp = resources.getXml(resourceValue)
-                while (xrp.eventType != XmlResourceParser.END_DOCUMENT) {
-                    if (xrp.eventType == 2) {
-                        try {
-                            val string = xrp.name
-                            if (componentName != null) {
-                                if (xrp.getAttributeValue(0).compareTo(componentName) == 0) {
-                                    resource = xrp.getAttributeValue(1)
-                                }
-                            } else if (string == resourceName) {
-                                resource = xrp.getAttributeValue(0)
-                            }
-                        } catch (e: Exception) {
-                            println(e)
-                        }
-
-                    }
-                    xrp.next()
-                }
+    private fun getIconDrawableName(resources: Resources,
+                                    packageName: String,
+                                    componentName: ComponentName): String? {
+        var result: String? = null
+        findInAppfilterXml(packageName, resources) {
+            if (it.name == "item"
+                    && it.attributeCount >= 2
+                    && it.getAttributeName(0) == "component"
+                    && it.getAttributeValue(0) == componentName.toString()
+                    && it.getAttributeName(1) == "drawable") {
+                result = it.getAttributeValue(1)
+                return@findInAppfilterXml true
+            } else {
+                return@findInAppfilterXml false
             }
-        } catch (e: Exception) {
-            println(e)
         }
+        return result
+    }
 
-        return resource
+    private fun getFallbackIconDrawableName(resources: Resources,
+                                            packageName: String,
+                                            elementName: String): String? {
+        var result: String? = null
+        findInAppfilterXml(packageName, resources) {
+            if (it.name == elementName
+                    && it.attributeCount >= 1
+                    && it.getAttributeName(0) == "img1") {
+                result = it.getAttributeValue(0)
+                return@findInAppfilterXml true
+            } else {
+                return@findInAppfilterXml false
+            }
+        }
+        return result
+    }
+
+    private fun getScaleFactor(resources: Resources,
+                               packageName: String): Float {
+        var result = 1.0f
+        findInAppfilterXml(packageName, resources) {
+            if (it.name == "scale"
+                    && it.attributeCount >= 1
+                    && it.getAttributeName(0) == "factor") {
+                result = it.getAttributeValue(0).toFloat()
+                return@findInAppfilterXml true
+            } else {
+                return@findInAppfilterXml false
+            }
+        }
+        return result
+    }
+
+    private fun findInAppfilterXml(packageName: String,
+                                   resources: Resources,
+                                   finder: (XmlResourceParser) -> Boolean) {
+        val resourceValue = resources.getIdentifier("appfilter", "xml", packageName)
+        if (resourceValue != 0) {
+            val xrp = resources.getXml(resourceValue)
+            while (xrp.eventType != XmlResourceParser.END_DOCUMENT) {
+                if (xrp.eventType == XmlResourceParser.START_TAG) {
+                    if (finder(xrp)) {
+                        return
+                    }
+                }
+                xrp.next()
+            }
+        }
     }
 
     private fun getResizeMatrix(bm: Bitmap,
