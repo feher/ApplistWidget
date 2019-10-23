c2fb449728befe77aef550ba028a0b5421c048a8
Gabor Fekete <feheren.fekete@gmail.com>
2019-10-23 22:20:15 +0300

    Fix crash.
    Handle nulls.

--- app/src/main/java/net/feheren_fekete/applist/applistpage/ApplistPageFragment.kt
+++ app/src/main/java/net/feheren_fekete/applist/applistpage/ApplistPageFragment.kt
@@ -208,8 +208,8 @@ class ApplistPageFragment : Fragment() {
             menu.findItem(R.id.action_search_app)?.isVisible = true
             menu.findItem(R.id.action_create_section)?.isVisible = false
             menu.findItem(R.id.action_settings)?.isVisible = false
-            menu.findItem(R.id.action_edit_pages).isVisible = false
-            menu.findItem(R.id.action_change_wallpaper).isVisible = false
+            menu.findItem(R.id.action_edit_pages)?.isVisible = false
+            menu.findItem(R.id.action_change_wallpaper)?.isVisible = false
         } else {
             menu.findItem(R.id.action_search_app)?.isVisible = true
             menu.findItem(R.id.action_create_section)?.isVisible = true
