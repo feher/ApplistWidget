6a67ac576de5c3ae01ef7ecb6f9d9d1d9a1ef92a
Gabor Fekete <feheren.fekete@gmail.com>
2020-04-25 15:07:31 +0300

    Fix Pixel effect shader
    Make Pixel effect resolution independent.

--- app/src/main/java/net/feheren_fekete/applist/applistpage/iconpack/loader/PixelIconPackLoader.kt
+++ app/src/main/java/net/feheren_fekete/applist/applistpage/iconpack/loader/PixelIconPackLoader.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.opengl.GLES20
+import android.util.Log
 import jp.co.cyberagent.android.gpuimage.GPUImage
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageFilter
 import net.feheren_fekete.applist.R
@@ -16,24 +17,24 @@ class PixelIconPackLoader(
 ): EffectIconPackLoader(context, packageManager, imageUtils) {
 
     init {
-        parameter = 15.0f
+        parameter = MIN_PARAM
     }
 
     override fun applyEffect(originalIcon: Bitmap, parameter: Float): Bitmap {
         val gpuImage = GPUImage(context)
-        gpuImage.setFilter(PixelFilter().apply { setPixel(parameter) })
+        gpuImage.setFilter(PixelFilter().apply { setPercent(parameter) })
         return gpuImage.getBitmapWithFilterApplied(originalIcon)
     }
 
     override fun parameterToFloat(intParameter: Int): Float {
         val percentage = intParameter.toFloat() / 100.0f
-        return 30.0f * percentage
+        return MIN_PARAM + (percentage * (MAX_PARAM - MIN_PARAM))
     }
 
     override fun isEditable() = true
 
     override fun getEditableParameter(): Int {
-        return ((parameter / 30.0f) * 100.0f).toInt()
+        return ((parameter - MIN_PARAM) / (MAX_PARAM - MIN_PARAM) * 100.0f).toInt()
     }
 
     private class PixelFilter: GPUImageFilter(
@@ -41,15 +42,13 @@ class PixelIconPackLoader(
         """
             precision highp float;
             varying vec2 textureCoordinate;
-            uniform float imageWidthFactor;
-            uniform float imageHeightFactor;
             uniform sampler2D inputImageTexture;
-            uniform float pixel;
+            uniform float percent;
             void main()
             {
               vec2 uv  = textureCoordinate.xy;
-              float dx = pixel * imageWidthFactor;
-              float dy = pixel * imageHeightFactor;
+              float dx = percent;
+              float dy = percent;
               vec2 coord = vec2(dx * floor(uv.x / dx), dy * floor(uv.y / dy));
               vec4 tc = texture2D(inputImageTexture, coord).rgba;
               gl_FragColor = tc;
@@ -58,31 +57,22 @@ class PixelIconPackLoader(
     ) {
         private var imageWidthFactorLocation = 0
         private var imageHeightFactorLocation = 0
-        private var pixel = 1.0f
-        private var pixelLocation = 0
+        private var percent = 0.1f
+        private var percentLocation = 0
 
         override fun onInit() {
             super.onInit()
-            imageWidthFactorLocation = GLES20.glGetUniformLocation(program, "imageWidthFactor")
-            imageHeightFactorLocation =
-                GLES20.glGetUniformLocation(program, "imageHeightFactor")
-            pixelLocation = GLES20.glGetUniformLocation(program, "pixel")
+            percentLocation = GLES20.glGetUniformLocation(program, "percent")
         }
 
         override fun onInitialized() {
             super.onInitialized()
-            setPixel(pixel)
+            setPercent(percent)
         }
 
-        override fun onOutputSizeChanged(width: Int, height: Int) {
-            super.onOutputSizeChanged(width, height)
-            setFloat(imageWidthFactorLocation, 1.0f / width)
-            setFloat(imageHeightFactorLocation, 1.0f / height)
-        }
-
-        fun setPixel(pixel: Float) {
-            this.pixel = pixel
-            setFloat(pixelLocation, this.pixel)
+        fun setPercent(pixel: Float) {
+            this.percent = pixel
+            setFloat(percentLocation, this.percent)
         }
     }
 
@@ -90,6 +80,9 @@ class PixelIconPackLoader(
         const val name = "pixel"
         const val displayName = "Pixel"
         const val displayIconId = R.drawable.icon_effect_pixel
+
+        private const val MIN_PARAM = 0.05f
+        private const val MAX_PARAM = 0.14f
     }
 
 }
