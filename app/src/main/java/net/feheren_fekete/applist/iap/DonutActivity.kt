087f9093ff4522f6923c2c12945cf3d1cb3995c3
Gabor Fekete <feheren.fekete@gmail.com>
2020-04-30 22:54:32 +0300

    Add spinner to Donut activity

--- app/src/main/java/net/feheren_fekete/applist/iap/DonutActivity.kt
+++ app/src/main/java/net/feheren_fekete/applist/iap/DonutActivity.kt
@@ -1,6 +1,7 @@
 package net.feheren_fekete.applist.iap
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -28,16 +29,25 @@ class DonutActivity: AppCompatActivity() {
             } else 0
         donutActivityRoot.setPadding(0, topPadding, 0, bottomPadding)
 
+        progressBar.visibility = View.VISIBLE
+        giveOneDonut.visibility = View.INVISIBLE
+        giveTwoDonuts.visibility = View.INVISIBLE
+        giveThreeDonuts.visibility = View.INVISIBLE
+
         viewModel = ViewModelProvider(this).get(DonutViewModel::class.java)
         viewModel.products().observe(this, Observer { products ->
             if (products == null) {
                 return@Observer
             }
             if (products.size >= 3) {
+                progressBar.visibility = View.GONE
+                giveOneDonut.visibility = View.VISIBLE
                 giveOneDonut.text = getString(R.string.donut_page_donut, products[0].price)
                 giveOneDonut.setOnClickListener { purchaseDonut(products[0]) }
+                giveTwoDonuts.visibility = View.VISIBLE
                 giveTwoDonuts.text = getString(R.string.donut_page_donut, products[1].price)
                 giveTwoDonuts.setOnClickListener { purchaseDonut(products[1]) }
+                giveThreeDonuts.visibility = View.VISIBLE
                 giveThreeDonuts.text = getString(R.string.donut_page_donut, products[2].price)
                 giveThreeDonuts.setOnClickListener { purchaseDonut(products[2]) }
             }
